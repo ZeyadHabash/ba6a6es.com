e129370a4e38fe16794d4cf15604c10065c7e3d7
Zeyadptd <zeyad.habashy02@gmail.com>
2022-11-25 22:15:57 +0200

    Implementing MongoDB and gitignoring the node modules folder

--- networksApp/app.js
+++ networksApp/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
 const { title } = require('process');
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
 var app = express();
 
 // view engine setup
@@ -61,8 +64,20 @@ app.get('/wanttogo', function(req,res){
 app.post('/', function(req,res){
   res.render('home', {title : "Home"})
 })
+
 app.post('/search', function(req,res){
   res.render('searchresults', {title : "Home"})
 })
+
+
+MongoClient.connect("mongodb://0.0.0.0:27017",function(err,client){
+  if (err) throw err;
+  var db = client.db("appDB");
+  db.collection('Users').insertOne({id: 1, firstName: 'abab', lastName: 'xyxy'});
+
+  db.collection('Users').find().toArray(function(err,results){
+    console.log(results);
+  });
+});
 
 app.listen(3000);
