83720b1592674fb5cc6a5b6e04d1bf79f04eb16e
Zeyadptd <zeyad.habashy02@gmail.com>
2022-11-26 22:17:39 +0200

    figuring out mongo + node

--- networksApp/app.js
+++ networksApp/app.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Get Requests 
 
+// Get Requests 
 app.get('/', function(req,res){
   res.render('login', {title: "Login"})
 });
@@ -69,15 +69,43 @@ app.post('/search', function(req,res){
   res.render('searchresults', {title : "Home"})
 })
 
+app.post('/register', function(req,res){
+  var usernameVar = req.body.username;
+  var passwordVar = req.body.password;
+  var exists = false;
+  MongoClient.connect("mongodb://0.0.0.0:27017",function(err,client){
+    if (err) throw err;
+    var db = client.db("appDB");
 
+    db.collection('Users').find().toArray(function(err,results){
+      for (let i = 0; i < results.length; i++){
+        console.log("inloop" + i);
+        if(results[i]["username"] == usernameVar){
+          exists = true;
+          break;
+        }
+      }
+      console.log(usernameVar);
+      if(!exists){
+        res.redirect('/');
+        console.log("doesnt exist");
+      }
+      else
+        console.log("exists!");
+      });
+  });
+})
+
+
+// Mongo stuff
 MongoClient.connect("mongodb://0.0.0.0:27017",function(err,client){
   if (err) throw err;
   var db = client.db("appDB");
-  db.collection('Users').insertOne({id: 1, firstName: 'abab', lastName: 'xyxy'});
+ // db.collection('Users').insertOne({username: 'abab', password: 'xyxy'});
 
-  db.collection('Users').find().toArray(function(err,results){
-    console.log(results);
-  });
+  // db.collection('Users').find().toArray(function(err,results){
+  //   console.log(results);
+  // });
 });
 
 app.listen(3000);
